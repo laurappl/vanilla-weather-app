d7c694bf3292a85dbf5b3987a30c42c4abfd1da8
laurappl <71591914+laurappl@users.noreply.github.com>
2020-11-08 13:57:49 +0100

    updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
+function formatDate() {
+    let now = new Date();
+    let date = now.getDate();
+    let hours = now.getHours();
+    if (hours < 10) {
+  hours = `0${hours}`;
+    }
+    let minutes = now.getMinutes ();
+    if (minutes < 10) {
+  
+  minutes = `0${minutes}`;
+  
+    }
+    let year = now.getFullYear();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ];
+    let day = days[now.getDay()];
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let month = months[now.getMonth()];
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = `${day}, ${date} ${hours}:${minutes}`;
+  }
+  
+  formatDate();
+
 function displayTemperature (response) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +55,7 @@ function displayTemperature (response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    
 }
 
 let apiKey = "ab133fb9369951af7d1e6bff8e77458d";
