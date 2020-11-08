aec82b762a331cca548ba30be889b49905d483c0
laurappl <71591914+laurappl@users.noreply.github.com>
2020-11-08 15:01:22 +0100

    added the relevant icon

--- src/index.js
+++ src/index.js
@@ -55,11 +55,18 @@ function displayTemperature (response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
-    
+    let iconData = response.data.weather[0].icon;
+    let iconLink = `http://openweathermap.org/img/wn/${iconData}@2x.png`;
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", iconLink);
+    iconElement.setAttribute("alt", `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png` );
+    // console.log(response.data);
 }
 
 let apiKey = "ab133fb9369951af7d1e6bff8e77458d";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=${units}`;
+let city = "Meilen";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(displayTemperature);
+
